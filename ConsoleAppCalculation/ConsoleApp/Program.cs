1dc753bd7b52fff0d0c20de29d4aff0fc50148c6
ipz222_tmo <ipz222_tmo@student.ztu.edu.ua>
2024-01-26 20:09:02 +0200

    Created ConsoleApp for Calculation. Task 3-8.

---
+++ ConsoleAppCalculation/ConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System.Text;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+
+        double a;
+        Console.Write("Введіть числа а = ");
+        double.TryParse(Console.ReadLine(), out a);
+
+        double b;
+        Console.Write("Введіть числа b = ");
+        double.TryParse(Console.ReadLine(), out b);
+
+        double summ = a + b;
+        Console.WriteLine($"\nСумма введедних чисел: {summ}");
+    }
+}
